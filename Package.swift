8a53101d214bfae74cef683dde367426908c1d21
WANG Jie <jacques09125715@gmail.com>
2024-12-18 12:04:12 +0100

    Bump version to 1.0.1

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CSCrashReporter",
-            url: "https://github.com/ContentSquare/CS_iOS_CrashReporter/releases/download/1.0.0/CSCrashReporter.xcframework.zip",
-            checksum: "fc35487b598136a47fcf1b2fea5350b2db4847dcf3e46241cfa25069d670efb2")
+            url: "https://github.com/ContentSquare/CS_iOS_CrashReporter/releases/download/1.0.1/CSCrashReporter.xcframework.zip",
+            checksum: "b6aad85a367281ed18ededf9437b2fa26a7dec688362f1e52e1c814b01b6756b")
     ]
 )
